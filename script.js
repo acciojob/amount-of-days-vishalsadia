ec0b1cd3ba921261f0c6ebdc99aba0d78add09d4
vishalsadia <125430740+vishalsadia@users.noreply.github.com>
2023-12-09 10:19:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 function dayOfAYear(year) {
-	if((year%4===0 && year%100!=0)||year%400===0)){
+	if((year%4===0 && year%100!=0)||(year%400===0)){
 		return 366;
 	}
 	else{
