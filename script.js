794f0992835b14da7c9edaefb41afa1354c064cd
vishalsadia <125430740+vishalsadia@users.noreply.github.com>
2023-12-09 10:21:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-//your JS code here. If required.
-function dayOfAYear(year) {
-	if((year%4===0 && year%100!=0)||(year%400===0)){
-		return 366;
-	}
-	else{
-		return 365;
-	}
-	
+function daysOfAYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        // Leap year: divisible by 4 and not divisible by 100 or divisible by 400
+        return 366;
+    } else {
+        // Non-leap year
+        return 365;
+    }
 }
+
+// Examples
+console.log(daysOfAYear(2022)); // Output: 365
+console.log(daysOfAYear(2024)); // Output: 366
+console.log(daysOfAYear(1900)); // Output: 365
+console.log(daysOfAYear(2000)); // Output: 366
